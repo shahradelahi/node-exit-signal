ad42ab80a10e29fcf56728f8e25e156caeeef96a
Shahrad Elahi <shahrad@litehex.com>
2025-03-01 20:53:41 +0000

    fix: using `deasync` for delay (#8)

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-export function delay(ms: number): void {
-  if (Number.isNaN(ms) || ms < 0) {
-    throw new Error('Expected a non-negative number');
-  }
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
-}
